9f3c5d4ca99be5666e818a036acdfdaf818b0232
Hernan-M <hernan.medeiross@gmail.com>
2023-12-20 12:25:37 -0300

    v1 without mobile

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,11 +1,46 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.css']
 })
 
+
 export class CarouselComponent {
 
+  falso = false;
+  vdd = true;
+
+  products: any;
+
+  responsiveOptions: any[] | undefined;
+
+  constructor() {}
+
+  ngOnInit() {
+          this.products = ['ADRIANA', 'CARLOS LIRA', 'IVAN LINS', 'JORGE BEN JOR',
+      'JOSÉ AUGUSTO', 'LUIZ MELODIA', 'MARCOS VALLE', 'PENINHA (SONHOS)', 'RAUL SEIXAS', 'ROSEMARY',
+    'SALLY BAWDWIN (UK)', 'SERGINHO ROUPA NOVA', 'SIDNEY MAGAL', 'TAIGUARA', 'THE FEVERS', 'TIM MAIA', 'WANDO']
+
+
+     this.responsiveOptions = [
+          {
+              breakpoint: '1199px',
+              numVisible: 1,
+              numScroll: 1
+          },
+          {
+              breakpoint: '991px',
+              numVisible: 2,
+              numScroll: 1
+          },
+          {
+              breakpoint: '767px',
+              numVisible: 1,
+              numScroll: 1
+          }
+      ];
+  }
 }
